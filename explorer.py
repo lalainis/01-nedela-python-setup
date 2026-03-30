4e533c93f7b10fac438131bd045c1e5bde939109
lalainis <lauris.alainis@gmail.com>
2026-03-30 18:46:40 +0300

    feat : jābūt salabotam

--- explorer.py
+++ explorer.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # virkņu savienošana
 print ("5"+"3")
 # print ("5"+3)
@@ -26,33 +25,4 @@ print (float("1e3"))
 #citi interesanti gadījumi
 print (0.1 + 0.2 == 0.3)
 print (round(2.5))
-=======
-# virkņu savienošana
-print ("5"+"3")
-# print ("5"+3)
-print (int("5")+3)
-#Robežgadījumi
-#print (int("abc")) - tāpēc ka "abc" nav skaitlis, un nevar tikt pārveidots par skaitli.
-print (float("3.14"))
-#truthy/Falsy vērtības
-print (bool(""))
-print (bool(" "))
-print (bool("0"))
-print (bool(0))
-print (bool([]))
-print (bool(None))
-print (True + True)
-#jauktā aritmētika
-print (True * 10)
-print (False + 5)
-print (10 / True)
-#skaitļu pārveidošana
-print (int(3.86))
-#print (int("3.14")) - tāpēc ka pēdiņas ir virkne un nevar tikt pārveidota par skaitli, un arī tāpēc ka "3.14" nav vesels skaitlis.
-print (int(float("3.14")))
-print (float("1e3"))
-#citi interesanti gadījumi
-print (0.1 + 0.2 == 0.3)
-print (round(2.5))
->>>>>>> d9a41b4966ee25839806f6f380f7e892465b0885
 print (round(3.5))  
