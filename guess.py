2cb13573cbce588029e571267f44ac5984357f1e
lalainis <lauris.alainis@gmail.com>
2026-04-02 11:40:10 +0300

    feat: pievienota minēšanas sadaļa

--- guess.py
+++ guess.py
@@ -2,4 +2,23 @@ import random
 
 # Generate a random number between 1 and 100
 random_number = random.randint(1, 100)
-print(f"Random number: {random_number}")
+attempts = 0
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+print("Can you guess what it is?")
+
+while True:
+    try:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if guess < random_number:
+            print("Too low! Try again.")
+        elif guess > random_number:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed it in {attempts} attempts.")
+            break
+    except ValueError:
+        print("Please enter a valid number.")
