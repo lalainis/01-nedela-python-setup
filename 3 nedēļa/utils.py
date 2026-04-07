582be1c32b0ede1384466d21260d8984ddde219a
lalainis <lauris.alainis@gmail.com>
2026-04-07 15:56:32 +0300

    feat: 2 clamps added

--- 3 nedēļa/utils.py
+++ 3 nedēļa/utils.py
@@ -5,7 +5,7 @@ def capitalize_first_letter(word):
 print(capitalize_first_letter("world"))
 
 def truncate(text, max_len=10):
-    """Truncē tekstu, saglabājot tikai pirmos max_len simbolus."""
+    """Apgriež tekstu, saglabājot tikai pirmos max_len simbolus."""
     return text[:max_len]
 print(truncate("Šis ir ļoti garš teksts, kas jātruncē.", 15))
 
@@ -13,3 +13,20 @@ def count_words(text):
     """Saskaita vārdus tekstā."""
     return len(text.split())
 print(count_words("Šis ir piemērs, lai saskaitītu vārdus manā mājas darbā."))
+
+def clamp(value, min_value, max_value):
+    """Ierobežo vērtību starp min_value un max_value."""
+    return max(min_value, min(value, max_value))
+print(clamp(-5, 0, 10))
+
+def clamp_list(values, min_value, max_value):
+    """Ierobežo katru vērtību sarakstā starp min_value un max_value.
+    
+    args:
+        num skaitis ko ierobežot :-6    
+        min_value minimālā vērtība :0
+        max_value maksimālā vērtība :12
+
+    returns: int vai float ierobežotā vērtība
+    """
+    return [clamp(value, min_value, max_value) for value in values]
