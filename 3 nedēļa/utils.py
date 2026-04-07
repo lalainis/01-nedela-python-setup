0b397bd79a4a2b97e3e36cc5c484281ee8324890
lalainis <lauris.alainis@gmail.com>
2026-04-07 16:48:57 +0300

    feat:skaitļu funkcija pievienota

--- 3 nedēļa/utils.py
+++ 3 nedēļa/utils.py
@@ -30,3 +30,34 @@ def clamp_list(values, min_value, max_value):
     returns: int vai float ierobežotā vērtība
     """
     return [clamp(value, min_value, max_value) for value in values]
+print(clamp_list([-5, 0, 5, 10, 15], 0, 10))
+
+def is_prime(num):
+    """Pārbauda, vai skaitlis ir pirmskaitlis."""
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+input_number = 8
+print(f"Vai {input_number} ir pirmskaitlis? {is_prime(input_number)}")
+
+def factorial(n):
+    """Aprēķina faktoriālu skaitlim n.
+    
+    args:
+        n skaitlis, kura faktoriālu aprēķināt : 6
+    returns: int faktoriāla vērtība
+    """
+    if n < 0:
+        raise ValueError("Faktoriāls nav definēts negatīviem skaitļiem.")
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+n = 6
+print(f"{n} faktoriāls ir: {factorial(n)}")
