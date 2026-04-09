a55c362fee8e35687f92ff1b097d3ff3cd1ed07e
lalainis <lauris.alainis@gmail.com>
2026-04-09 14:24:41 +0300

    feat: sākotnējais stroage .py fails

--- 04-NEDELA-json uzdevumi/shoping_list/storage.py
+++ 04-NEDELA-json uzdevumi/shoping_list/storage.py
@@ -1,3 +1,9 @@
+import json
+import sys
+import os
+
+LIST_FILE = "shopping.json"
+
 def load_list():
     """Ielādē iepirkumu sarakstu no JSON faila."""
     if not os.path.exists(LIST_FILE):
@@ -9,3 +15,4 @@ def save_list(items):
     """Saglabā iepirkumu sarakstu JSON failā."""
     with open(LIST_FILE, "w", encoding="utf-8") as f:
         json.dump(items, f, ensure_ascii=False, indent=2)
+        
